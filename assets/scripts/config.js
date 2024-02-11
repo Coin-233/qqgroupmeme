6f7739b93e12f85628d06a45ef65f57f5a503d92
Coin-233 <Coin-233@users.noreply.github.com>
2024-02-11 06:07:00 +0000

    Rename Coin-233/qqgroupmeme@fe53f7f105b8d25e376e1a4c20932161eebb58ad

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,11 +2,11 @@ export default {
   items: [
     "pic/梦开始的地方.png",
     "pic/剑酱的🐕 2.png",
+    "pic/管理虚哩.jpg",
     "pic/剑酱的🐕.png",
     "pic/gfx现在是个烧货.png",
     "pic/让我当❤.png",
     "pic/可爱.png",
     "pic/被冷水强碱了.jpg",
-    "pic/管理虚哩.JPG",
   ]
 }
