d4032b6203c8a7f785c0250b2e3033db1158ad29
GoForceX <GoForceX@users.noreply.github.com>
2024-02-13 13:41:16 +0000

    Add 小币崽子 Coin-233/qqgroupmeme@ef38327f8d746c452f99f180428fe0d3633e4976

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,18 +1,19 @@
 export default {
   items: [
-    "pic/长名字大战小男孩.png",
     "pic/梦开始的地方.png",
     "pic/剑酱的🐕 2.png",
-    "pic/断章取义.png",
-    "pic/已经是剑酱的东西了.png",
-    "pic/触手play.png",
-    "pic/被床撅晕.png",
-    "pic/管理虚哩.jpg",
-    "pic/剑酱的🐕.png",
-    "pic/gfx现在是个烧货.png",
     "pic/让我当❤.png",
+    "pic/gfx现在是个烧货.png",
+    "pic/触手play.png",
+    "pic/小币崽子.png",
     "pic/可爱.png",
     "pic/被冷水强碱了.jpg",
+    "pic/管理虚哩.jpg",
     "pic/有人打炮.png",
+    "pic/长名字大战小男孩.png",
+    "pic/已经是剑酱的东西了.png",
+    "pic/断章取义.png",
+    "pic/剑酱的🐕.png",
+    "pic/被床撅晕.png",
   ]
 }
